0d3c4c8389095e9740e37475959f508501963919
team806 <118126987+team806@users.noreply.github.com>
2023-02-22 15:37:16 -0500

    New version with spark max

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,28 +7,28 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import com.ctre.phoenix.sensors.AbsoluteSensorRange;
 import com.ctre.phoenix.sensors.CANCoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
-import edu.wpi.first.math.kinematics.SwerveModulePosition;
-import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.ADIS16470_IMU.IMUAxis;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
+ * project.
+ */
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
@@ -37,33 +37,78 @@ public class Robot extends TimedRobot {
   // controller
   private final XboxController controller = new XboxController(0);
   // gyro
-  //public static final ADIS16470_IMU IMU = new ADIS16470_IMU();
+  public static final ADIS16470_IMU IMU = new ADIS16470_IMU();
   // PIDs
   PIDController pid = new PIDController(0.02, 0, 0);
   PIDController ballancePID = new PIDController(0.02, 0, 0);
   PIDController driveXPID = new PIDController(0.02, 0, 0);
   PIDController driveYPID = new PIDController(0.02, 0, 0);
   PIDController driveAngPID = new PIDController(0.02, 0, 0);
+  // Talons
+  
+  private final WPI_TalonFX motor_FRmag = new WPI_TalonFX(2);
+  private final WPI_TalonFX motor_FLmag = new WPI_TalonFX(4);
+  private final WPI_TalonFX motor_RRmag = new WPI_TalonFX(6);
+  private final WPI_TalonFX motor_RLmag = new WPI_TalonFX(7);
 
-  //Compressor compressor = new Compressor(1, PneumaticsModuleType.CTREPCM); 
-  //DoubleSolenoid exampleDoublePCM = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 1, 0);
+  //sparks
+  private final CANSparkMax motor_FRang = new CANSparkMax(3,MotorType.kBrushless );
+  private final CANSparkMax motor_FLang = new CANSparkMax(8, MotorType.kBrushless);
+  private final CANSparkMax motor_RRang = new CANSparkMax(1, MotorType.kBrushless);
+  private final CANSparkMax motor_RLang = new CANSparkMax(5, MotorType.kBrushless);
 
 
-  SwerveDriveKinematics m_kinematics;
-  SwerveDriveOdometry m_odometry;
+  // CANCoders
+  CANCoder FR_coder = new CANCoder(9);
+  CANCoder FL_coder = new CANCoder(11);
+  CANCoder RR_coder = new CANCoder(10);
+  CANCoder RL_coder = new CANCoder(12);
+  //modules
+  //swerveModule FR_module = new swerveModule(motor_FRmag,motor_FRang,FR_coder);
+  //swerveModule FL_module = new swerveModule(motor_FLmag,motor_FLang,FL_coder);
+  //swerveModule RR_module = new swerveModule(motor_RRmag,motor_RRang,RR_coder);
+  //swerveModule RL_module = new swerveModule(motor_RLmag,motor_RLang,RL_coder);
+  // encoder position values
+  double FR_coderPosition;
+  double FL_coderPosition;
+  double RR_coderPosition;
+  double RL_coderPosition;
+  // constants
+  protected double swerveRatio = -0.36;
+  // motor speed maximums
+  protected double angSpeedMax = 0.3;
+  protected double magSpeedMax = 1 - (angSpeedMax * -swerveRatio);
+  // x,y position of the robot in feet and degrees
+  double robotX = 0;
+  double robotY = 0;
+  double gyroYaw;
+  // clockwise gyro yaw
+  double CWgyroYaw;
+  // velocity of robot in feet per 20ms
+  double velocityX;
+  double velocityY;
+  //
+  boolean onChargeStation;
+  // modifiers for controller inputs (not for drive or module drive methods)
+  double controllerMagMax = 0.8;
+  double controllerMagPow = 3;
+  // target 2D pose values for
+  double targetRobotAng;
+  double targetRobotY;
+  double targetRobotX;
 
-
+  /**
+   * This function is run when the robot is first started up and should be used
+   * for any
+   * initialization code.
+   */
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
 
-    
-    //compressor.enableAnalog(kDefaultPeriod, kDefaultPeriod);
-
-    pid.enableContinuousInput(90, 100);
-    /*
+    pid.enableContinuousInput(0, 360);
     FR_coder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
     FR_coder.configSensorDirection(true);
     FR_coder.configMagnetOffset(102);
@@ -76,45 +121,92 @@ public class Robot extends TimedRobot {
     RL_coder.configAbsoluteSensorRange(AbsoluteSensorRange.Unsigned_0_to_360);
     RL_coder.configSensorDirection(true);
     RL_coder.configMagnetOffset(-28);
-    */
-    //IMU.calibrate();
-    //IMU.setYawAxis(IMUAxis.kZ);
-    
-    //modules
-    swerveModule FR_module = new swerveModule(new motor(new WPI_TalonFX(3)),new motor(new WPI_TalonFX(2)),new CANCoder(9));
-    swerveModule FL_module = new swerveModule(new motor(new WPI_TalonFX(8)),new motor(new WPI_TalonFX(4)),new CANCoder(11));
-    swerveModule RR_module = new swerveModule(new motor(new WPI_TalonFX(1)),new motor(new WPI_TalonFX(6)),new CANCoder(10));
-    swerveModule RL_module = new swerveModule(new motor(new WPI_TalonFX(5)),new motor(new WPI_TalonFX(7)),new CANCoder(12));
-    
+    IMU.calibrate();
+    IMU.setYawAxis(IMUAxis.kZ);
 
-    m_kinematics = new SwerveDriveKinematics(
-      new Translation2d(0.3175,0.3175),
-      new Translation2d(-0.3175,0.3175),
-      new Translation2d(0.3175,-0.3175),
-      new Translation2d(-0.3175,-0.3175)
-    );
-    /*
-    SwerveDriveOdometry m_odometry = new SwerveDriveOdometry(
-      m_kinematics, 
-      new Rotation2d(Math.toRadians(IMU.getAngle())),
-      new SwerveModulePosition[] {FR_module.getPosition(),FL_module.getPosition(),RR_module.getPosition(),RL_module.getPosition()}, 
-      new Pose2d(0, 0,new Rotation2d())
-    );
-  */
+    SmartDashboard.putNumber("angSpeedMax", angSpeedMax);
+    SmartDashboard.putNumber("magSpeedMax", magSpeedMax);
+
+    SmartDashboard.putNumber("controller Mag Max", controllerMagMax);
   }
 
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for
+   * items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and
+   * test.
+   *
+   * <p>
+   * This runs after the mode specific periodic functions, but before LiveWindow
+   * and
+   * SmartDashboard integrated updating.
+   */
   @Override
   public void robotPeriodic() {
-/*
-    m_odometry.update(
-      new Rotation2d(Math.toRadians(IMU.getAngle())),
-      new SwerveModulePosition[] {FR_module.getPosition(),FL_module.getPosition(),RR_module.getPosition(),RL_module.getPosition()}
-    );
-*/
-
-
+    // CCW = positive, centered to NORTH
+    gyroYaw = IMU.getAngle();
+    // CW = positive, centered to EAST
+    CWgyroYaw = -(gyroYaw + 90);
+    // CW = positive, centered to EAST
+    FR_coderPosition = FR_coder.getAbsolutePosition();
+    FL_coderPosition = FL_coder.getAbsolutePosition();
+    RR_coderPosition = RR_coder.getAbsolutePosition();
+    RL_coderPosition = RL_coder.getAbsolutePosition();
+    /*
+     * double velocityX = ((wheelSpeed(motor_FRmag) * Math.cos(CWgyroYaw +
+     * FR_coderPosition)) +
+     * (wheelSpeed(motor_FLmag) * Math.cos(CWgyroYaw + FL_coderPosition)) +
+     * (wheelSpeed(motor_RRmag) * Math.cos(CWgyroYaw + RR_coderPosition)) +
+     * (wheelSpeed(motor_RLmag) * Math.cos(CWgyroYaw + RL_coderPosition))) / 4;
+     * double velocityY = ((wheelSpeed(motor_FRmag) * Math.sin(CWgyroYaw +
+     * FR_coderPosition)) +
+     * (wheelSpeed(motor_FLmag) * Math.sin(CWgyroYaw + FL_coderPosition)) +
+     * (wheelSpeed(motor_RRmag) * Math.sin(CWgyroYaw + RR_coderPosition)) +
+     * (wheelSpeed(motor_RLmag) * Math.sin(CWgyroYaw + RL_coderPosition))) / 4;
+     * 
+     * robotX += velocityX;
+     * robotY += velocityY;
+     * 
+     * SmartDashboard.putNumber("X velocity", velocityX);
+     * SmartDashboard.putNumber("Y velocity", velocityY);
+     * SmartDashboard.putNumber("X", robotX);
+     * SmartDashboard.putNumber("Y", robotY);
+     * SmartDashboard.putNumber("Yaw", gyroYaw);
+     */
+    /*
+     * SmartDashboard.putNumber("FR codeer ang", FR_coderPosition);
+     * SmartDashboard.putNumber("FL codeer ang", FL_coderPosition);
+     * SmartDashboard.putNumber("RR codeer ang", RR_coderPosition);
+     * SmartDashboard.putNumber("RL codeer ang", RL_coderPosition);
+     * 
+     * SmartDashboard.putNumber("FR ang motor", motor_FRang.get());
+     * SmartDashboard.putNumber("FL ang motor", motor_FLang.get());
+     * SmartDashboard.putNumber("RR ang motor", motor_RRang.get());
+     * SmartDashboard.putNumber("RL ang motor", motor_RLang.get());
+     * SmartDashboard.putNumber("FR mag motor", motor_FRmag.get());
+     * SmartDashboard.putNumber("FL mag motor", motor_FLmag.get());
+     * SmartDashboard.putNumber("RR mag motor", motor_RRmag.get());
+     * SmartDashboard.putNumber("RL mag motor", motor_RLmag.get());
+     */
   }
-   
+
+  /**
+   * This autonomous (along with the chooser code above) shows how to select
+   * between different
+   * autonomous modes using the dashboard. The sendable chooser code works with
+   * the Java
+   * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the
+   * chooser code and
+   * uncomment the getString line to get the auto name from the text box below the
+   * Gyro
+   *
+   * <p>
+   * You can add additional auto modes by adding additional comparisons to the
+   * switch structure
+   * below with additional strings. If using the SendableChooser make sure to add
+   * them to the
+   * chooser code above as well.
+   */
   @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
@@ -123,6 +215,7 @@ public class Robot extends TimedRobot {
 
   }
 
+  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
     switch (m_autoSelected) {
@@ -136,75 +229,100 @@ public class Robot extends TimedRobot {
     }
   }
 
+  /** This function is called once when teleop is enabled. */
   @Override
   public void teleopInit() {
 
   }
 
+  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
-    /* 
-    drive(
-      new ChassisSpeeds(
-        controller.getLeftX(), 
-        controller.getLeftY(), 
-        controller.getRightX()
-      )
-    );
+    controllerMagMax = SmartDashboard.getNumber("controller Mag Max", controllerMagMax);
 
-    if (controller.getYButtonPressed()) {IMU.reset();}
-    */
-  }
-/* 
-  private void drive(ChassisSpeeds inputSpeeds) {
-    SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(inputSpeeds);
-    FR_module.setModule(SwerveModuleState.optimize(moduleStates[1],new Rotation2d(FR_coder.getAbsolutePosition())));
-    FL_module.setModule(SwerveModuleState.optimize(moduleStates[2],new Rotation2d(FL_coder.getAbsolutePosition())));
-    RR_module.setModule(SwerveModuleState.optimize(moduleStates[3],new Rotation2d(RR_coder.getAbsolutePosition())));
-    RL_module.setModule(SwerveModuleState.optimize(moduleStates[4],new Rotation2d(RL_coder.getAbsolutePosition())));
-  }
-  */
+    drive(controller.getLeftX(),controller.getLeftY(),Math.pow(controller.getRightX(),controllerMagPow));
 
+    if (controller.getAButtonPressed()) {
+      IMU.reset();
+    }
+  }
+
+  /**
+   * @return Drives the robot in specified direction, rotates input according to
+   *         gyroYaw
+   * @param x one to negitive one value for driving left or right respectivly
+   * @param y one to negitive one value for driving back or forward respectivly
+   * @param z one to negitive one value for driving counter clockwise or clockwise
+   *          respectivly
+   */
+  private void drive(double x, double y, double z) {
+    double controlerAng = Math.atan2(y, x) + Math.toRadians(gyroYaw + 90);// rotate by gyro for field
+    double controlermag = Math.pow(MathUtil.clamp(Math.hypot(x, y), -1, 1), controllerMagPow) * controllerMagMax;
+    x = Math.cos(controlerAng) * controlermag;
+    y = Math.sin(controlerAng) * controlermag;
+
+    moduleDrive(motor_FRang, motor_FRmag, FR_coderPosition, x + (z * -0.707106), y + (z * 0.707106));
+    moduleDrive(motor_FLang, motor_FLmag, FL_coderPosition, x + (z * 0.707106), y + (z * 0.707106));
+    moduleDrive(motor_RRang, motor_RRmag, RR_coderPosition, x + (z * -0.707106), y + (z * -0.707106));
+    moduleDrive(motor_RLang, motor_RLmag, RL_coderPosition, x + (z * 0.707106), y + (z * -0.707106));
+
+    SmartDashboard.putNumber("final x", x);
+    SmartDashboard.putNumber("final y", y);
+  }
+
+  /**
+   * @return Drive a swerve module acording to an x and y value. NOT field
+   *         oriended
+   * @param motor_FRang2   the steering motor of the respective module
+   * @param magMotor   the steering motor of the respective module
+   * @param encoderAng the encoder ang of the respective module
+   * @param x          one to negitive one value for driving left or right
+   *                   respectivly
+   * @param y          one to negitive one value for driving back or forward
+   *                   respectivly
+   */
+  void moduleDrive(CANSparkMax motor_FRang2, WPI_TalonFX magMotor, double encoderAng, double x, double y) {
+    double targetAng = Math.toDegrees(Math.atan2(y, x));
+    double targetMag = Math.hypot(y, x) / (1 + Math.hypot(0.707106, 0.707106));// scaled to range [-1,1]
+    double setang;
+    double setmag;
+
+    if (distance(encoderAng, targetAng) > 90) {
+      setang = targetAng + 180;
+      setmag = -targetMag;
+    } else {
+      setang = targetAng;
+      setmag = targetMag;
+    }
+
+    motor_FRang2.set(pid.calculate(encoderAng, setang) * angSpeedMax);
+    magMotor.set((setmag * magSpeedMax) + (swerveRatio * motor_FRang2.get()));
+  }
+
+  public static double distance(double alpha, double beta) {
+    double phi = Math.abs(beta - alpha) % 360; // This is either the distance or 360 - distance
+    double distance = phi > 180 ? 360 - phi : phi;
+    return distance;
+  }
+
+  /** This function is called once when the robot is disabled. */
   @Override
   public void disabledInit() {
   }
 
+  /** This function is called periodically when disabled. */
   @Override
   public void disabledPeriodic() {
   }
 
+  /** This function is called once when test mode is enabled. */
   @Override
   public void testInit() {
   }
 
+  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
-
-    //SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(new ChassisSpeeds(controller.getLeftX(),controller.getLeftY(),-controller.getRightX()));
-    SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(getSetVelocity(controller));
     
-    SmartDashboard.putNumber("FR module ang", -moduleStates[0].angle.getDegrees() -90);
-    SmartDashboard.putNumber("FR module speed", moduleStates[0].speedMetersPerSecond);
-    SmartDashboard.putNumber("FL module ang", -moduleStates[1].angle.getDegrees() -90);
-    SmartDashboard.putNumber("FL module speed", moduleStates[1].speedMetersPerSecond);
-    SmartDashboard.putNumber("RR module ang", -moduleStates[2].angle.getDegrees() -90);
-    SmartDashboard.putNumber("RR module speed", moduleStates[2].speedMetersPerSecond);
-    SmartDashboard.putNumber("RL module ang", -moduleStates[3].angle.getDegrees() -90);
-    SmartDashboard.putNumber("RL module speed", moduleStates[3].speedMetersPerSecond);
-
-
-    /*if (controller.getAButtonPressed()) {
-      exampleDoublePCM.set(Value.kForward);
-    }else{
-      exampleDoublePCM.set(Value.kReverse);
-    }
-*/
-  }
-  private ChassisSpeeds getSetVelocity(XboxController controller) {
-    double x = controller.getLeftX();
-    double y = controller.getLeftY();
-    double speed = MathUtil.clamp(Math.pow(Math.hypot(x,y),3),0,1);
-    double ang = Math.atan2(y, x);
-    return new ChassisSpeeds(Math.cos(ang)*speed,Math.sin(ang)*speed,controller.getRightX());
   }
 }
